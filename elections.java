81e5d22d249890fa4e1a2adc9e89149b8c45654c
vamika19singh <104374671+vamika19singh@users.noreply.github.com>
2023-01-18 19:55:28 +0530

    Update elections.java

--- elections.java
+++ elections.java
@@ -7,7 +7,6 @@ public class ElectionStatistics {
 	HashMap<String, Integer> map = new HashMap<String, Integer>();
 	
 	public ElectionStatistics() {
-		
 	}
 	
 	public static void main(String []args) {
@@ -42,49 +41,17 @@ public class ElectionStatistics {
 		
 		}
 	}
-
-	private void getWinner() {
+	private void createCandidate(Scanner sc) {
 		
-		int result = Integer.MIN_VALUE;
-		String candidateName = null;
-	
-		for(Map.Entry<String,Integer> entry : map.entrySet()) {
-			
-			if(entry.getValue() > result) {
-				candidateName = entry.getKey();
-				result = entry.getValue();
-			}
-		}
-		
-		System.out.println("Winner: "+ candidateName + "Total votes: "+ result);	
-	}
-
-	private void listVote() {
-		// TODO Auto-generated method stub
-		System.out.println("Results");
-		
-		for(Map.Entry<String,Integer> entry : map.entrySet()) {
-			System.out.println("Candidate: "+ entry.getKey() + "Vote"+ entry.getValue());
-		}	
-	}
-
-	private void countVote(Scanner sc) {
-		// TODO Auto-generated method stub
-		System.out.println("Please enter candidateName to get vote count");
+		System.out.println("Enter candidate Name");
 		String candidateName = sc.next();
+		map.put(candidateName, 0);
 		
-		if(map.containsKey(candidateName)) {
-			
-			System.out.println(candidateName + " votes "+ map.get(candidateName));
-		} else {
-			System.out.println(candidateName + " is not a valid candidate");
-		}	
 	}
-
 	private void castVote(Scanner sc) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Please enter candidate Name to cast vote");
+		System.out.println("Name of candidate to cast vote: ");
 		String candidateName = sc.next();
 		
 		if(map.containsKey(candidateName)) {
@@ -95,12 +62,42 @@ public class ElectionStatistics {
 		}
 		
 	}
-
-	private void createCandidate(Scanner sc) {
-		
-		System.out.println("Please enter candidate Name");
+	private void countVote(Scanner sc) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter candidate Name to get vote count: ");
 		String candidateName = sc.next();
-		map.put(candidateName, 0);
 		
+		if(map.containsKey(candidateName)) {
+			
+			System.out.println(candidateName + " votes "+ map.get(candidateName));
+		} else {
+			System.out.println(candidateName + " is not a valid candidate");
+		}	
 	}
+
+	private void listVote() {
+		// TODO Auto-generated method stub
+		System.out.println("Results");
+		
+		for(Map.Entry<String,Integer> entry : map.entrySet()) {
+			System.out.println("Candidate: "+ entry.getKey() + "Vote"+ entry.getValue());
+		}	
+	}
+	
+	private void getWinner() {
+		
+		int winner = Integer.MIN_VALUE;
+		String candidateName = null;
+	
+		for(Map.Entry<String,Integer> entry : map.entrySet()) {
+			
+			if(entry.getValue() > winner) {
+				candidateName = entry.getKey();
+				winner = entry.getValue();
+			}
+		}
+		
+		System.out.println("Winner: "+ candidateName + "Total votes: "+ winner);	
+	}
+	
 }
